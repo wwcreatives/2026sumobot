daa74b544d4f637bba5f4728d06f8b06a326f94f
William Wu <162932564+wwcreatives@users.noreply.github.com>
2026-09-14 21:58:26 +0800

    added pin assignments for robot (should be final)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <NewPing.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LEFT_BUTTON 0
+#define RIGHT_BUTTON 14
+
+#define ENA 17
+#define IN1 1
+#define IN2 2
+#define IN3 21
+#define IN4 10
+#define ENB 18
+#define TRIG1 12
+#define ECHO1 11
+#define TRIG2 16
+#define ECHO2 13
+
+bool leftButton = false;
+bool rightButton = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  bool leftButton = !digitalRead(LEFT_BUTTON);
+  bool rightButton = !digitalRead(RIGHT_BUTTON);
 }
